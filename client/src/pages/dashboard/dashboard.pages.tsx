93d0009c396d01bbad5fb35e0dc1c79359e0f976
Siddhant Prateek <siddhantprateek@gmail.com>
2022-08-06 21:26:21 +0530

    discover page

--- client/src/pages/dashboard/dashboard.pages.tsx
+++ client/src/pages/dashboard/dashboard.pages.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { VOLCA } from '../../assets';
+import './dashboard.styles.css';
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div className='discover-dash'>
+      <img className='disc-bg' src={VOLCA} alt="" />
+      <div className="grad-bg"></div>
+      <div className="search-container">
+        <input type="text" placeholder='Search for Places, Plants, Animals....' className="search-field" />
+      </div>
+    </div>
   )
 }
 
