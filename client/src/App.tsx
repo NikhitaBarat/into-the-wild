0c4b9121341a5107708f256c5a52e0d44f13876c
suvangipaul <suvangipaul8@gmail.com>
2022-08-06 12:05:56 +0530

    Routed path

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
-import './App.css';
-import { Home } from './pages';
-import { Routes, Route } from 'react-router-dom'
+import React from "react";
+import "./App.css";
+import { Dashboard, Home, PlantProfile, Tribeprofile } from "./pages";
+import { Routes, Route } from "react-router-dom";
+import { Header } from "./components";
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route index element={<Home />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="plantprofile" element={<PlantProfile />} />
+        <Route path="tribeprofile" element={<Tribeprofile />} />
       </Routes>
     </div>
   );
