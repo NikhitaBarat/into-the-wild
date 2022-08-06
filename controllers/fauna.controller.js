3685687538e53c27de08a8e5e8f80f6be90399c9
Siddhant Prateek <siddhantprateek@gmail.com>
2022-08-06 16:10:55 +0530

    plants controller

--- controllers/fauna.controller.js
+++ controllers/fauna.controller.js
@@ -1,14 +1,52 @@
+const FaunaModel = require('../models/flaura.model'); 
+
 // @desc fetch all the plant at that location
 // @route GET
+const getPlants = async (req, res) => {
+    try {
 
+
+    }catch(error){
+        res.send(error);
+    }
+}
 // @desc create plant data
 // @route POST
+const createPlant = async (req, res) => {
+    try{
+
+    }catch(error){
+        res.send(error);
+    }
+}
 
 // @desc fetch single plant data
 // @route GET
+const getSinglePlant = async (req, res) => {
+    try{
+
+    }catch(error){
+        res.send(error);
+    }
+}
 
 // @desc delete plant data
 // @route DELETE
+const deletePlant = async (req, res) => {
+    try{
+
+    }catch(error){
+        res.send(error);
+    }
+}
 
 // @desc update plant details
 // @route PUT
+
+const updatePlant = async (req, res) => {
+    try{
+
+    }catch(error){
+        res.send(error);
+    }
+}
