785a7c1bbc6402d789b70bb29fd539f4b686fcb3
Siddhant Prateek <siddhantprateek@gmail.com>
2022-08-06 10:27:59 +0530

    controllers description added

---
+++ controllers/tribe.controller.js
@@ -0,0 +1,17 @@
+
+
+// @desc fetch all the tribe at that location
+// @route GET
+
+// @desc create tribe data
+// @route POST
+
+// @desc fetch single tribe data
+// @route GET
+
+// @desc delete tribe data
+// @route DELETE
+
+// @desc update tribe details
+// @route PUT
+
